30d4e7b1842bfb52d8f3b59600e8b4ae06f55c47
Dirk Wenzel <der-wenzel@gmx.de>
2019-05-27 13:02:56 +0200

    [RELEASE] Release of geo_location_service 0.1.0 (Dirk Wenzel)

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
   'author_company' => '',
-  'version' => '0.0.1',
+  'version' => '0.1.0',
   'constraints' =>
   array (
     'depends' =>
