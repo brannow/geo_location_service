de0901978a1ad4aa4e88ba770a611ee17c16e488
Elias Häußler <e.haeussler@familie-redlich.de>
2022-03-28 16:21:52 +0200

    [TASK] Declare additional compatibility with TYPO3 10.4 and 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.3.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.0-11.5.99',
         ],
         'suggests' => [
             'pagenotfoundhandling' => '',
