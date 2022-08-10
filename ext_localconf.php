edf4908bdcc68464c827a600086660f537628578
Benjamin Rannow <b.rannow@familie-redlich.de>
2022-08-10 14:40:22 +0200

    php8.1 fix

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,18 +4,15 @@ defined('TYPO3_MODE') or die();
 
 // Register geo location cache
 (function () {
-    $cacheConfigurations = &$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'];
-
-    if (!is_array($cacheConfigurations[\CPSIT\GeoLocationService\Cache\GeoLocationCache::NAME])) {
-        $cacheConfigurations[\CPSIT\GeoLocationService\Cache\GeoLocationCache::NAME] = [];
-    }
-    if (!isset($cacheConfigurations[\CPSIT\GeoLocationService\Cache\GeoLocationCache::NAME]['backend'])) {
-        $cacheConfigurations[\CPSIT\GeoLocationService\Cache\GeoLocationCache::NAME]['backend']
+    $cacheConfigurations = $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\CPSIT\GeoLocationService\Cache\GeoLocationCache::NAME]??[];
+    if (!isset($cacheConfigurations['backend'])) {
+        $cacheConfigurations['backend']
             = \TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class;
     }
-    if (!isset($cacheConfigurations[\CPSIT\GeoLocationService\Cache\GeoLocationCache::NAME]['options'])) {
-        $cacheConfigurations[\CPSIT\GeoLocationService\Cache\GeoLocationCache::NAME]['options'] = [
+    if (!isset($cacheConfigurations['options'])) {
+        $cacheConfigurations['options'] = [
             'defaultLifetime' => \CPSIT\GeoLocationService\Cache\GeoLocationCache::DEFAULT_LIFETIME,
         ];
     }
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\CPSIT\GeoLocationService\Cache\GeoLocationCache::NAME] = $cacheConfigurations;
 })();
